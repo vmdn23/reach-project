783d26780f561ca8accc875dfdf7e360a84d2ae3
vmdn23 <victormdnguyen88@gmail.com>
2019-10-12 00:21:28 -0700

    add python 3.7 install script

---
+++ scripts/install_python3.7.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# script to install python 3.7
+
+
+# download python3.7
+sudo cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+
+# unzip the tarball
+sudo tar xf Python-3.7.3.tar.xz
+cd Python-3.7.3.tar.xz
+
+# run the configuration file
+sudo ./configure --enable-optimizations --with-ensurepip=install
+sudo make altinstall
+
+# upgrade pip
+sudo pip3.7 install --upgrade pip
+
