feb3cd407d5658e0147988cf53fc47b2a610d883
vmdn23 <victormdnguyen88@gmail.com>
2019-10-14 22:40:23 -0700

    create v2, add regex filter for letters and special characs, change word list to use api

---
+++ hangman_v2.py
@@ -0,0 +1,166 @@
+# Hangman game
+
+
+import random
+import re
+from api_words import get_words
+
+
+# Filters inputs to only allow letters
+input_filter = re.compile('\w+')
+# Filters out special characters
+special_filter = re.compile('^.[~!@#$%^&()_+={}[]|:;“’<,>.?๐฿].$')
+
+
+# Grabs random word using the api_words function from the word dictionary api 
+word_list = get_words()
+random.shuffle(word_list)
+secret_word = word_list.pop().upper()
+
+
+correct = []
+incorrect = []
+
+
+hangman_board = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+
+
+# Display word to check if underscores are working
+print(f"Debugger: {secret_word} ")
+
+
+def draw_game_board():
+    # Draws the hangman ascii as well as the word display board
+    print(hangman_board[len(incorrect)])
+    print("\n\n")
+    for i in secret_word:
+        if i in correct:
+            print(i, end=' ')
+        else:
+            print('_', end=' ')
+    print("\n\n")
+    print("********* YOUR GUESSES SO FAR *********")
+    for i in incorrect:
+        print(i, end=' ')
+    print("\n***************************************\n")
+
+
+def user_guess():
+    # Let's the user take a guess. Appends the letter to correct or inncorrect
+    while True:
+        pre_guess = input("Type in your guess\n: ").upper()
+        guess = input_filter.search(pre_guess)
+        specials = special_filter.search(pre_guess) 
+
+        # If there is an empty guess or there isn't any specail characters
+        if guess is None or specials is not None:
+            print("\nPlease enter a proper guess.\n")
+            continue
+        
+        # There is no group() in a none, reassign to fix error
+        guess = guess.group()
+
+        if specials is not None:
+            specials = specials.group()
+
+        elif guess in correct or guess in incorrect:
+            print("\nYou've already guessed that! Try again.\n")
+
+        elif guess.isnumeric():
+            print("\nPlease enter letters and not numbers. Try again.\n")
+            
+        elif guess == secret_word:
+            break
+        
+        else:
+            break
+
+
+        """ Add condition to prevent users from making big string of guess"""
+
+
+    if guess == secret_word or guess in secret_word:
+        correct.append(guess)
+    else:
+        incorrect.append(guess)
+
+
+def win_checker():
+    # Checks to see if the user has won or lost the current game.        
+    if len(incorrect) > 5:
+        return 'lose'
+    if correct == []:
+        return 'not yet'
+    for i in secret_word:
+        if i not in correct[0]:
+            return 'not yet'      
+    return 'win'
+
+
+    """ NEED to add trigger to win when user completes guesses """
+
+
+while True:
+    draw_game_board()
+    user_guess()
+    game_status = win_checker()
+
+    if game_status == 'lose':
+        print(hangman_board[6])
+        print("\n~~~ GAME OVER ~~~")
+        print(f"The secret word was ~~~ {secret_word} ~~~")
+        break
+    elif game_status == 'win':
+        print("\n~~~~~ YOU WON THE GAME! ~~~~~")
+        print(f"The secret word was ~~~ {secret_word} ~~~")
+        break
