266f18f14cd1f68a6410a2f6937d68322271abb6
vmdn23 <victormdnguyen88@gmail.com>
2019-10-16 15:48:01 -0700

    initialize test file with plans for tests

---
+++ test_hangman_v2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Hangman tests """
+
+# from hangman_v2 import draw_game_board
+# from hangman_v2 import user_guess
+# from hangman_v2 import win_checker
+
+from hangman_v2 import *
+import mock
+import module
+
+## Main function tests
+
+# def test_draw_game_board(capysys):
+
+
+# def test_user_guess_():
+#     pass
+
+# def test_win_checker():
+
+#def_play_again():
+
+
+## Inital step up tests
+# test_gets_word_from_api():
+
+
+## More display tests
+# def test_letter_correctly_gets_displayed_when_guessed()
+
+
+
+## Game condition
+# test_user_wins_game():
+
+# test_user_loses_game():
+
+# test_user_ends_game_with_no_replay():
+
+# test_user_replays_game():
+
