1180091c724dcc025556aef81f1223351e3c6697
vmdn23 <victormdnguyen88@gmail.com>
2019-10-17 10:18:41 -0700

    add more possible test conditions

--- test_hangman.py
+++ test_hangman.py
@@ -5,30 +5,29 @@
 # from hangman_v2 import user_guess
 # from hangman_v2 import win_checker
 
-from hangman_v2 import *
+from hangman import *
 import mock
 import module
 
 ## Main function tests
-
 # def test_draw_game_board(capysys):
 
-
 # def test_user_guess_():
-#     pass
 
 # def test_win_checker():
 
 #def_play_again():
 
 
+
 ## Inital step up tests
+# test_api_reponse_200():
+
 # test_gets_word_from_api():
 
 
 ## More display tests
 # def test_letter_correctly_gets_displayed_when_guessed()
-
 
 
 ## Game condition
